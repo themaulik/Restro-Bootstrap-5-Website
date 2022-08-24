3346f2ecafee2d16038bdc428df0b3132575166c
maulik <maulik@maulikgoswami.com>
2022-08-24 19:00:17 +0530

    Fixed the text and js

--- src/js/main.js
+++ src/js/main.js
@@ -1,31 +1,32 @@
-function validate(){
+function validate() {
     var name = document.myForm.name.value;
     var numbers = /^\+?([0-9]{2})\)?[-. ]?([0-9]{4})[-. ]?([0-9]{4})$/;
-      
-    if(name==""){
+
+    if (name == "") {
         alert("Please Fell Your 'Name'");
         return false;
     }
-    if (document.myForm.email.value=="") {
+    if (document.myForm.email.value == "") {
         alert("please Provide Email");
         return false;
     }
-    if(number.value.match(numbers)){
+    if (number.value.match(numbers)) {
         return true;
-    }else{
+    } else {
         alert("You Have Enter More Then 10 Digite");
         return false;
     }
 }
 
-function validateEmail(){
-    var emialId = document.myForm.email.value;
-    atpos = emialId.indexOf("@");
-    dotpos = emialId.lastIndexOf(".");
+ function validateEmail(){
+     var emialId = document.myForm.email.value;
+     atpos = emialId.indexOf("@");
+     dotpos = emialId.lastIndexOf(".");
 
-    if (atpos<1 || (dotpos-atpos<2)) {
-        alert("Please Enter Correct Email Id");
-        return false;
-    }
-    return true;
-}
+     if (atpos<1 || (dotpos-atpos<2)) {
+         alert("Please Enter Correct Email Id");
+         document.myForm.email.focus();
+         return false;
+     }
+     return true;
+ }
